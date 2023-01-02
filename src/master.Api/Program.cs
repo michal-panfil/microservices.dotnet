0f141d9db8f8c620e1edd602b8fc5d27a32fce2d
Michał Panfil <it.panfil@gmail.com>
2023-01-02 21:54:02 +0100

    rabitmq client

--- src/master.Api/Program.cs
+++ src/master.Api/Program.cs
@@ -1,4 +1,6 @@
 
+using master.Infrastructure.HostedServices;
+
 namespace master.Api;
 
 public class Program
@@ -8,6 +10,7 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
+        builder.Services.AddHostedService<MessageReceiverService>();
 
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

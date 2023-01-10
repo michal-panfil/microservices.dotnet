7d340a52f9cb09151da415ad4e57a473f9013924
Michał Panfil <it.panfil@gmail.com>
2023-01-10 22:32:23 +0100

    extract db creation to infrastructure project

--- src/master.Api/Program.cs
+++ src/master.Api/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using master.Infrastructure.Services;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using master.Infrastructure;
 
 namespace master.Api;
 
@@ -23,7 +24,7 @@ public class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-        builder.Services.AddDbContext<OrderContext>(x => x.UseSqlite(@"Data Source=C:\db\orderingMaster.db"));
+        ServiceExtensions.AddDatabase(builder.Configuration, builder.Services);
         builder.Services.AddSingleton(() => new JsonSerializerOptions(JsonSerializerDefaults.Web)
 
         {
---
+++ src/master.Infrastructure/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using master.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace master.Infrastructure
+{
+    public static class ServiceExtensions
+    {
+        public static void AddDatabase(IConfiguration configuration, IServiceCollection services)
+        {
+            services.AddDbContext<OrderContext>(x => x.UseSqlite(@"Data Source=C:\db\orderingMaster.db"));
+        }
+    }
+}

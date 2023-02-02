3662695f0ad90c52f625a465e0733cab069698a0
Michał Panfil <it.panfil@gmail.com>
2023-02-02 22:27:03 +0100

    ui singalr adjustment

--- src/myeshop/src/app/services/shipment-service.ts
+++ src/myeshop/src/app/services/shipment-service.ts
@@ -4,8 +4,8 @@ import * as signalR from "@microsoft/signalr"
   providedIn: 'root'
 })
 export class SignalrService {
-  public data: any[] = [];
-  public bradcastedData: any[] = [];
+  public shipmentId: number = 0;
+  public shipmentUpdate: ShipmentUpdate = { shipmentId: 0, remainingKm: 0, currentLocation: '' };
   private hubConnection: signalR.HubConnection;
     
     constructor() { 
@@ -20,27 +20,16 @@ export class SignalrService {
         .then(() => console.log('Connection started'))
         .catch(err => console.log('Error while starting connection: ' + err))
     }
-    
-    public addTransferChartDataListener = () => {
-      this.hubConnection.on('transferchartdata', (data) => {
-        this.data = data;
-        console.log(data);
-      });
-    }
-    public broadcastChartData = () => {
-      const data = this.data.map(m => {
-        const temp = {
-          data: m.data,
-          label: m.label
-        }
-        return temp;
-      });
-      this.hubConnection.invoke('broadcastchartdata', data)
-      .catch(err => console.error(err));
-    }
-    public addBroadcastChartDataListener = () => {
-      this.hubConnection.on('broadcastchartdata', (data) => {
-        this.bradcastedData = data;
+   
+    public addShipmentStatusListener = () => {
+      this.hubConnection.on('shipmentstatus', (data) => {
+        this.shipmentUpdate = data;
       })
     }
+}
+
+export interface ShipmentUpdate {
+  shipmentId: number;
+  remainingKm: number;
+  currentLocation: string;
 }
